8ea5289439fd9b29f47f711c684488dc9a94cba6
Marcin Bieniek <marcingim@gmail.com>
2023-01-17 21:42:18 +0100

    Add Annoucment component

--- app/src/components/pages/Home/Home.js
+++ app/src/components/pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Annoucment from '../../views/Annoucment/Annoucment'
 import Navbar from '../../views/Navbar/Navbar'
 
 const Home = () => {
   return (
     <div>
+      <Annoucment />
       <Navbar />
     </div>
   )
---
+++ app/src/components/views/Annoucment/Annoucment.js
@@ -0,0 +1,11 @@
+import styles from './Annoucment.module.scss';
+
+const Annoucment = () => {
+  return (
+    <div className={styles.container}>
+      Best offer! Buy 2 pairs of shoes, get 3rd for free!
+    </div>
+  )
+}
+
+export default Annoucment
