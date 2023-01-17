4417fb68a5404babcbb35e930501614da40cb8f0
Marcin Bieniek <marcingim@gmail.com>
2023-01-17 21:15:36 +0100

    Add basic files and navbar component

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import Home from './components/pages/Home/Home';
+import './styles/global.scss';
+
+function App() {
+  return (
+    
+    <Home />
+
+  ); 
+}
+
+export default App;
---
+++ app/src/components/views/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from './Navbar.module.scss';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        
+        <div className={styles.left}>
+          <span className={styles.language}>EN</span>
+          <div className={styles.container_search}>
+            <input className={styles.input_search}></input>
+            <Search className={styles.icon_search} />
+          </div>
+        </div>
+        
+        <div className={styles.center}>
+          <h1 className={styles.logo}>RUNNER.</h1>
+        </div>
+        
+        <div className={styles.right}>
+          <div className={styles.menu_item}>REGISTER</div>    
+          <div className={styles.menu_item}>SIGN IN</div>
+          <div className={styles.menu_item}>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>    
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
