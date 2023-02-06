3b874baa989f589dcab8d9cb00c17606bde8368f
Marcin Bieniek <marcingim@gmail.com>
2023-02-06 15:24:00 +0100

    Add stripe backend connection - everything works fine

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const orderRoute = require("./routes/order")
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
-const cartRoute = require("./routes/cart")
-
-dotenv.config(); 
+const cartRoute = require("./routes/cart");
+const stripeRoute = require("./routes/stripe")
+const cors = require("cors");
 
 mongoose.connect(
   process.env.MONGO_URL
@@ -18,13 +18,15 @@ mongoose.connect(
     console.log(err)
 });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
+app.use("/api/checkout", stripeRoute);
 
-app.listen(process.env.URL || 8082, () => {
+app.listen(process.env.URL || 8083, () => {
   console.log("Backend server is running!")
 })
--- backend/routes/stripe.js
+++ backend/routes/stripe.js
@@ -9,9 +9,9 @@ router.post("/payment", (req, res) => {
             currency: "usd"
         },
         (stripeErr, stripeRes) => {
-            if (stripeErr) {
+            if(stripeErr){
                 res.status(500).json(stripeErr);
-            } else {
+            }else{
                 res.status(200).json(stripeRes);
             }
         }
