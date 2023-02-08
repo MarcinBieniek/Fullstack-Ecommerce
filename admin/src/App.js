c5ed8bd338d9dd1176d825f73445270194453d77
Marcin Bieniek <marcingim@gmail.com>
2023-02-08 02:43:38 +0100

    Add sidebar component

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,17 @@
+import { style } from '@mui/system';
 import Topbar from './components/views/Topbar/Topbar';
 import './styles/global.scss';
+import styles from './App.module.scss'
+import Sidebar from './components/views/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <div className={styles.container}>
+        <Sidebar />
+        <div className={styles.others}>other pages</div>
+      </div>
     </div>
   );
 }
---
+++ admin/src/components/views/Sidebar/Sidebar.js
@@ -0,0 +1,101 @@
+import styles from "./Sidebar.module.scss"
+import LineStyleIcon from '@mui/icons-material/LineStyle';
+import TimelineIcon from '@mui/icons-material/Timeline';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import ReportIcon from '@mui/icons-material/Report';
+
+const Sidebar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        
+        <div className={styles.menu}>
+          <h3 className={styles.menu_title}>Dashboard</h3>
+          <ul className={styles.sidebar_list}>
+            <li className={styles.sidebar_listitem}>
+              <LineStyleIcon className={styles.sidebar_icon}/>
+              Home
+            </li>
+            <li className={styles.sidebar_listitem}>
+              <TimelineIcon className={styles.sidebar_icon}/>
+              Analitics
+            </li>
+            <li className={styles.sidebar_listitem}>
+              <TrendingUpIcon className={styles.sidebar_icon}/>
+              Sales
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.menu}>
+          <h3 className={styles.menu_title}>Quick menu</h3>
+          <ul className={styles.sidebar_list}>
+            <li className={styles.sidebar_listitem}>
+              <PermIdentityIcon className={styles.sidebar_icon} />
+              Users
+            </li>
+            <li className={styles.sidebar_listitem}>
+              <StorefrontIcon className={styles.sidebar_icon} />
+              Products
+            </li>
+            <li className={styles.sidebar_listitem}>
+              <AttachMoneyIcon className={styles.sidebar_icon} />
+              Transactions
+            </li>
+            <li className={styles.sidebar_listitem}>
+              <BarChartIcon className={styles.sidebar_icon} />
+              Reports
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.menu}>
+          <h3 className={styles.menu_title}>Notifications</h3>
+          <ul className={styles.sidebar_list}>
+            <li className={styles.sidebar_listitem}>
+              <MailOutlineIcon className={styles.sidebar_icon} />
+              Mail
+            </li>
+            <li className={styles.sidebar_listitem}>
+              <DynamicFeedIcon className={styles.sidebar_icon} />
+              Feedback
+            </li>
+            <li className={styles.sidebar_listitem}>
+              <ChatBubbleOutlineIcon className={styles.sidebar_icon}/>
+              Messages
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.menu}>
+          <h3 className={styles.menu_title}>Staff</h3>
+          <ul className={styles.sidebar_list}>
+            <li className={styles.sidebar_listitem}>
+              <WorkOutlineIcon className={styles.sidebar_icon}/>
+              Manage
+            </li>
+            <li className={styles.sidebar_listitem}>
+              <TimelineIcon className={styles.sidebar_icon}/>
+              Analytics
+            </li>
+            <li className={styles.sidebar_listitem}>
+              <ReportIcon className={styles.sidebar_icon}/>
+              Reports
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
