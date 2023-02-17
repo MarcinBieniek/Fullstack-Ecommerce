eb1026a809cd299bea29a5dfad392a0366730aac
Marcin Bieniek <marcingim@gmail.com>
2023-02-17 13:21:29 +0100

    Add calculate revenue box function

--- admin/src/components/features/FeaturedInfo/FeaturedInfo.js
+++ admin/src/components/features/FeaturedInfo/FeaturedInfo.js
@@ -1,20 +1,44 @@
 import styles from './FeaturedInfo.module.scss';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-
-import React from 'react'
+import { userRequest } from '../../../middleware/requestMethods';
+import { useState, useEffect } from 'react'
 
 const FeaturedInfo = () => {
+
+  const [income, setIncome] = useState([])
+  const [perc, setPerc] = useState(0)
+
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const res = await userRequest.get("orders/income") // it returns income of this and previous month
+        setIncome(res.data)
+        setPerc((res.data[1].total*100) / res.data[0].total - 100)
+      } catch(err) {
+        console.log('getIncome error is ', err)
+      }
+    }
+    getIncome();
+  }, []);
+
+  console.log('perc', perc)
+  console.log('income', income[1].total)
+
   return (
     <div className={styles.container}>
       
       <div className={styles.featured_item}>
         <span className={styles.featured_title}>Revanue</span>
         <div className={styles.featured_money_container}>
-          <span className={styles.featured_money}>$2,41</span>
+          <span className={styles.featured_money}>${income[1].total}</span>
           <span className={styles.featured_money_rate}>
-            -11,4 
-            <ArrowDownwardIcon className={`${styles.featured_icon} ${styles.negative}`} />
+            %{perc} 
+            { perc < 0 
+            ? <ArrowDownwardIcon className={`${styles.featured_icon} ${styles.negative}`} />
+            : <ArrowUpwardIcon className={styles.featured_icon}/>
+            }
+            
           </span>     
         </div>
         <span className={styles.featured_sub}>Compared to last month</span>
